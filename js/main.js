0a0e0d630da30272102b7c2a102e726955dbe1bb
Omar Meneses <fernandomenesesda@gmail.com>
2023-06-03 18:21:40 -0500

    ok

---
+++ js/main.js
@@ -0,0 +1,56 @@
+function circulo(){
+    let figura=document.getElementById("figura");
+
+    figura.classList.toggle("circulo");
+    // LA FUNCION TOGGLE  agrega y quita segun los click
+    //classlist hace referencia a todas 
+}
+function rectangulo(){
+    let figura=document.getElementById("figura");
+
+    figura.classList.toggle("rectangulo");
+}
+function degradado(){
+    let figura=document.getElementById("figura");
+
+    figura.classList.toggle("degradado");
+}
+function gif(){
+    let figura=document.getElementById("figura");
+
+    figura.classList.toggle("gif");
+}
+function arriba(){
+    const figura=document.getElementById("figura");
+
+    figura.classList.toggle("arriba");
+}
+function abajo(){
+    const figura=document.getElementById("figura");
+
+    figura.classList.toggle("abajo");
+}
+function derecha(){
+    const figura=document.getElementById("figura");
+    figura.classList.toggle("derecha");
+}
+function izquierda(){
+    const figura=document.getElementById("figura");
+    figura.classList.toggle("izquierda");
+}
+function diagonal(){
+    const figura=document.getElementById("figura");
+    figura.classList.toggle("diagonal");
+}
+function animation(){
+    const figura=document.getElementById("figura");
+    figura.classList.toggle("animation");
+}
+function panel(){
+    const panel = document.getSelector(".panel");
+    figura.classList.toggle("active");
+}
+function modal(){
+    const modal = document.getElementById("#modal");
+    modal.classList.toggle("active");
+}
